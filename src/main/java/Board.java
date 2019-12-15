8be7c2e98c06b1b650cdd5b70926649d1879b56c
Quentin Guenther <qguenther@outlook.com>
2019-12-14 22:41:08 -0800

    stub board methods

---
+++ src/main/java/Board.java
@@ -0,0 +1,14 @@
+public class Board {
+
+    public void update(int x, int y, int value) {
+
+    }
+
+    public void isValid(int x, int y, int value) {
+
+    }
+
+    public boolean isSolved() {
+        return false;
+    }
+}
---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class BoardTest {
+
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+    }
+}
