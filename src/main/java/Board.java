b2cd2bb42abf3eae059e740099fe4084dd67bcb4
Quentin Guenther <qguenther@outlook.com>
2019-12-15 17:35:04 -0800

    create basic updating and reading methods

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,7 +1,14 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Board {
+    public final int LENGTH = 9;
+    public Node[][] matrix = new Node[LENGTH][LENGTH];
 
     public void update(int x, int y, int value) {
-
+        matrix[x][y].setData(value);
     }
 
     public void isValid(int x, int y, int value) {
@@ -10,5 +17,93 @@ public class Board {
 
     public boolean isSolved() {
         return false;
+    }
+
+    public Map<Node, Collection<Node>> getState() {
+        Map<Node, Collection<Node>> matrixList = new HashMap<>();
+
+        for (Node[] nodeArr : matrix){
+            for(Node node : nodeArr) {
+                matrixList.put(node, node.getConnections().values());
+            }
+        }
+
+        return Collections.unmodifiableMap(matrixList);
+    }
+
+    public void generateMatrix() {
+        for (int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = new Node();
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                appendHorizontal(matrix[i][j], i);
+                appendVertical(matrix[i][j], j);
+                appendSquare(matrix[i][j], i, j);
+            }
+        }
+    }
+
+    private void appendHorizontal(Node node, int index) {
+        for(int i = 0; i < matrix[index].length; i++) {
+            node.addConnection(matrix[index][i]);
+        }
+    }
+
+    private void appendVertical(Node node, int index) {
+        for (Node[] nodes : matrix) {
+            node.addConnection(nodes[index]);
+        }
+    }
+
+    private void appendSquare(Node node, int row, int col) {
+        final double SQUARE_SIZE = Math.sqrt(LENGTH);
+
+        int r = row - row % 3;
+        int c = col - col % 3;
+
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                node.addConnection(matrix[i][j]);
+            }
+        }
+    }
+
+    public static class Node {
+        private int data;
+        private Map<Integer, Node> connections;
+
+        public Node() {
+            connections = new HashMap<>();
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public boolean isConnectedToValue(int value) {
+            for(Node node : connections.values()) {
+                if(node.getData() == value) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public Map<Integer, Node> getConnections() {
+            return this.connections;
+        }
+
+        public void addConnection(Node node) {
+            this.connections.put(node.hashCode(), node);
+        }
     }
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,6 +1,11 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class BoardTest {
 
@@ -9,5 +14,39 @@ class BoardTest {
     @BeforeEach
     void setUp() {
         board = new Board();
+    }
+
+    @Test
+    void nodesShouldNotShareHashcode() {
+        Board.Node node1 = new Board.Node();
+        Board.Node node2 = new Board.Node();
+
+        assertAll(() -> {
+            assertNotEquals(node1, node2);
+            assertNotEquals(node1.hashCode(), node2.hashCode());
+        });
+
+    }
+
+    @Test
+    void matrixShouldGenerateWithConnectedNodes() {
+        board.generateMatrix();
+
+        assertAll(() -> {
+            assertTrue(board.matrix[0][0].getConnections().containsValue(board.matrix[0][1]));
+            assertTrue(board.matrix[0][0].getConnections().containsValue(board.matrix[1][0]));
+            assertTrue(board.matrix[0][0].getConnections().containsValue(board.matrix[1][0]));
+            assertFalse(board.matrix[0][0].getConnections().containsValue(board.matrix[1][4]));
+        });
+    }
+
+    @Test
+    void connectedNotesShouldRecogniseStateChange() {
+        board.generateMatrix();
+        board.update(0, 1, 1);
+
+        assertEquals(1, board.matrix[0][1].getData());
+        assertTrue(board.matrix[0][0].isConnectedToValue(1));
+        assertFalse(board.matrix[0][0].isConnectedToValue(2));
     }
 }
